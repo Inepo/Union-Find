eb4ae8c330f63e2edf9c5290f3f784732073eadb
ines <ineskadjii@gmail.com>
2023-10-14 14:22:17 +0200

    Réalisation des premières méthodes demandées : Union, Find, addPeople et isolated.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        UnionFind u = new UnionFind(4);
+        int r = u.find(3);
+        System.out.println(r);
+
+        u.union(2,3);
+        u.union(1,3);
+
+        int t = u.find(1);
+
+        u.addPeople();
+        u.addPeople();
+        u.isolated(3);
     }
 }
---
+++ src/UnionFind.java
@@ -0,0 +1,78 @@
+public class UnionFind <T> {
+
+    private int[] amis;
+    private int nbHabitant;
+
+    public UnionFind(int nbH){
+        this.nbHabitant = nbH;
+        this.amis = new int[this.nbHabitant];
+        for(int i = 0; i < this.nbHabitant; ++i){
+            this.amis[i] = i;
+        }
+    }
+
+    public void union (int habitant1, int habitant2){
+        int r1 = habitant1;
+        while(this.amis[r1] != r1){
+            r1 = this.amis[r1];
+        }
+        int r2 = habitant2;
+        while(this.amis[r2] != r2){
+            r2 = this.amis[r2];
+        }
+        this.amis[r1] = r2;
+    }
+
+    public int find (int habitant){
+        int r = habitant;
+        while(this.amis[r] != r){
+            r = this.amis[r];
+        }
+        return r; // Determine le représentant de l'habitant
+    }
+
+    public void addPeople(){
+        ++this.nbHabitant;
+        int[] amistmp = new int[this.nbHabitant];
+        for(int i = 0; i < this.nbHabitant - 1; ++i){
+            amistmp[i] = this.amis[i];
+        }
+        this.amis = new int[this.nbHabitant];
+        for(int i = 0; i < this.nbHabitant - 1; ++i){
+             this.amis[i] = amistmp[i];
+        }
+
+        this.amis[this.nbHabitant-1] = this.nbHabitant-1;
+    }
+
+    /*public void isolated(int Habitant){
+        this.amis[Habitant] = Habitant;
+        for(int i = 0; i < this.nbHabitant; ++i){
+            if (this.amis[i] == Habitant && i != Habitant){
+                int min = i;
+                if(min >= i){
+                    min = i;
+                }
+                this.amis[i] = i;
+            }
+        }
+    }*/
+    public void isolated(int Habitant) {
+        boolean firstOccurrenceFound = false;
+        int min = 0;
+
+        for (int i = 0; i < this.nbHabitant; ++i) {
+            if (this.amis[i] == Habitant && i != Habitant) {
+                if (!firstOccurrenceFound) {
+                    min = i;
+                    firstOccurrenceFound = true;
+                }
+                this.amis[i] = min;
+            }
+        }
+    }
+
+
+
+
+}
