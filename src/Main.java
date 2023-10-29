75ce867ff35ca948f8447616aed073dccf9c7ade
isra <isradh1978@gmail.com>
2023-10-29 12:30:39 +0100

    test temps

--- src/Main.java
+++ src/Main.java
@@ -3,77 +3,8 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        /*
-        UnionFind u = new UnionFind(11);
 
-        System.out.println(u.toString());
-
-        int r = u.find(0);
-        System.out.println(r);
-
-        u.union(3, 10);
-        u.union(7,3);
-        u.union(4,7);
-        u.union(2,5);
-        u.union(10,4);
-        u.union(1,2);
-        System.out.println(u.toString());
-
-
-        System.out.println("Le représentant de 2 est : " + u.find(2));
-        System.out.println("Le représentant de 10 est : " + u.find(10));
-        System.out.println("Le représentant de 3 est : " +u.find(3));
-        System.out.println("Le représentant de 8 est : " +u.find(8));
-
-        u.addPeople();
-        u.addPeople();
-        u.addPeople();
-        u.addPeople();
-        u.addPeople();
-
-        System.out.println(u.find(12));
-
-        u.union(10,12);
-        u.union(5,12);
-        u.union(12,14);
-
-        System.out.println(u.toString());
-
-        u.isolated(3);
-        System.out.println(u.toString());
-
-        HashMap<String, String> k = new HashMap<>();
-
-        /*UnionFind v = new UnionFind(6);
-        System.out.println(v.toString());
-
-        v.union(3,5);
-        v.union(1,3);
-        v.union(0,1);
-
-        System.out.println(v.toString());
-
-        v.find(5);
-
-        System.out.println(v.toString());*/
-
-        /*u.union(1,3);
-
-        int t = u.find(3);
-        System.out.println(t);
-
-        System.out.println(u.toString());
-
-        u.addPeople();
-        u.addPeople();
-
-        System.out.println(u.toString());*/
-
-/*
-
-
-        ///Test compléxité moyenne en temps de la méthode Union
-        final int MIN = 10_000_000, MAX = 50_000_000, PAS = 10_000_000;
+        final int MIN = 2_000_000, MAX = 10_000_000, PAS = 2_000_000;
         final int MIN1 = 200_000, MAX1 = 1_000_000, PAS1 = 200_000;
 
 
@@ -81,43 +12,86 @@ public class Main {
         Random r2 = new Random();
         for (int n = MIN; n <= MAX; n += PAS) {
             UnionFind u = new UnionFind(n);
-            long tempsUnion = 0;
-            long tempsFind =0;
-            //int NB = 100;
-            int NB = r2.nextInt(n) + 1;
+
+            int j;
+
+
             System.out.println("Pour une taille " + n + " : ");
-            int j = 0;
-            for( int i = MIN1; i <= MAX1; i+=PAS1){
-                for(j = 0; j<i; j++){
+            //Methode Union
+            for (int i = MIN1; i <= MAX1; i += PAS1) {
+                long tempsUnion = 0;
+                for (j = 0; j < i; j++) {
                     int hab1 = r.nextInt(n);
                     int hab2 = r.nextInt(n);
                     long deb = System.nanoTime();
-                    u.union(hab1,hab2);
+                    u.union(hab1, hab2);
                     long fin = System.nanoTime();
                     tempsUnion += (fin - deb);
 
-                    //MESURER FIND
-                    int hab3 =  r.nextInt(n);
-                    long start = System.nanoTime();
-                    u.find(hab3);
-                    long end = System.nanoTime();
-                    tempsFind += (end - start);
                 }
-                /*if(j < i-1)
-                    System.out.print(i + " Union \t" + (tempsUnion)/1e6 + " ");
-                else
-                    System.out.println(i + " Union \t" + (tempsUnion)/1e6 + " ");
 
-
-                if(j < i-1)
-                    System.out.print(i + " find \t" + (tempsFind)/1e6 + " ");
-                else
-                    System.out.println(i + " find \t" + (tempsFind)/1e6 + " ");
+                System.out.println(i + " Union \t" + (tempsUnion) / 1e6 + " ");
             }
-          }*/
+
+            //Methode Find
+            for (int i = MIN1; i <= MAX1; i += PAS1) {
+                long tempsFind = 0;
+
+                for (j = 0; j < i; j++) {
+                    int hab1 = r.nextInt(n);
+                    int hab2 = r.nextInt(n);
+                    long deb = System.nanoTime();
+                    u.union(hab1, hab2);
+                    long fin = System.nanoTime();
+                    tempsFind += (fin - deb);
+                }
+
+                System.out.println(i + " Find \t" + (tempsFind) / 1e6 + " ");
+            }
+
+            //Methode Addpeople
+            for (int i = MIN1; i <= MAX1; i += PAS1) {
+                long tempsAddpeople = 0;
+                for (j = 0; j < i; j++) {
+                    int hab1 = r.nextInt(n);
+                    int hab2 = r.nextInt(n);
+                    long deb = System.nanoTime();
+                    u.union(hab1, hab2);
+                    long fin = System.nanoTime();
+                    tempsAddpeople += (fin - deb);
+                }
+
+                System.out.println(i + " Addpeople \t" + (tempsAddpeople) / 1e6 + " ");
+            }
+
+        }
+
+        // methode isolate
+
+        final int MINI = 100_000, MAXI = 500_000, PASI = 100_000;
+        final int MINIS = 1_000, MAXIS = 5_000, PASIS = 1_000;
+        for (int n = MINI; n <= MAXI; n += PASI) {
+            long tempsIsolate = 0;
+            UnionFind u = new UnionFind(n);
+            for (int i = MINIS; i <= MAXIS; i += PASIS) {
+
+                for (int j = 0; j < i; j++) {
+                    int hab = r.nextInt(n);
+                    long start = System.nanoTime();
+                    u.isolated(hab);
+                    long end = System.nanoTime();
+                    tempsIsolate += (end - start);
+                }
+
+                System.out.println(i + " Isolate \t" + (tempsIsolate) / 1e6 + "ms");
+            }
+        }
+
 
         /*********************** DEBUT TEST MEMOIRE OPTIMISE ***********************/
-        final int MIN = 100_000, MAX = 500_000, PAS = 100_000;
+        /*
+        final int MIN = 2_000_000, MAX = 10_000_000, PAS = 2_000_000;
+
         final int MINREP = 1_000, MAXREP = 5_000, PASREP = 1_000;
 
         Random r = new Random();
@@ -272,5 +246,5 @@ public class Main {
             }
           }
         */
-    }
-}
+            }
+        }
