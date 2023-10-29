0691290365bda0c86e5efc2ddeb88fb595053c39
isra <isradh1978@gmail.com>
2023-10-29 14:01:17 +0100

    nvl version

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,21 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-/*
-
+        /*********************** DEBUT TEST TEMPS ***********************/
         final int MIN = 2_000_000, MAX = 10_000_000, PAS = 2_000_000;
-        final int MIN1 = 200_000, MAX1 = 1_000_000, PAS1 = 200_000;
+        final int MINREPTEMPS = 200_000, MAXREPTEMPS = 1_000_000, PASREPTEMPS = 200_000;
 
 
         Random r = new Random();
-        Random r2 = new Random();
-        for (int n = MIN; n <= MAX; n += PAS) {
+        int j;
+        int n;
+        int i;
+        for (n = MIN; n <= MAX; n += PAS) {
             UnionFind u = new UnionFind(n);
-
-            int j;
-
-
             System.out.println("Pour une taille " + n + " : ");
-            //Methode Union
-            for (int i = MIN1; i <= MAX1; i += PAS1) {
+
+            //Mesure de la méthode Union -> Unir des habitants
+            for (i = MINREPTEMPS; i <= MAXREPTEMPS; i += PASREPTEMPS) {
                 long tempsUnion = 0;
                 for (j = 0; j < i; j++) {
                     int hab1 = r.nextInt(n);
@@ -27,234 +25,107 @@ public class Main {
                     u.union(hab1, hab2);
                     long fin = System.nanoTime();
                     tempsUnion += (fin - deb);
-
                 }
-
-                System.out.println(i + " Union \t" + (tempsUnion) / 1e6 + " ");
+                System.out.println(i + " Union \t" + (tempsUnion) / 1e6 + "ms");
             }
 
-            //Methode Find
-            for (int i = MIN1; i <= MAX1; i += PAS1) {
+            //Mesure de la méthode Find -> Trouver le représentant
+            for (i = MINREPTEMPS; i <= MAXREPTEMPS; i += PASREPTEMPS) {
                 long tempsFind = 0;
-
                 for (j = 0; j < i; j++) {
                     int hab1 = r.nextInt(n);
-                    int hab2 = r.nextInt(n);
                     long deb = System.nanoTime();
-                    u.find(hab1, hab2);
+                    u.find(hab1);
                     long fin = System.nanoTime();
                     tempsFind += (fin - deb);
                 }
-
-                System.out.println(i + " Find \t" + (tempsFind) / 1e6 + " ");
+                System.out.println(i + " Find \t" + (tempsFind) / 1e6 + "ms");
             }
 
-            //Methode Addpeople
-            for (int i = MIN1; i <= MAX1; i += PAS1) {
+            //Mesure de la méthode AddPeople -> Ajout d'habitant
+            for (i = MINREPTEMPS; i <= MAXREPTEMPS; i += PASREPTEMPS) {
                 long tempsAddpeople = 0;
                 for (j = 0; j < i; j++) {
-                    int hab1 = r.nextInt(n);
-                    int hab2 = r.nextInt(n);
                     long deb = System.nanoTime();
-                    u.addPeople(hab1, hab2);
+                    u.addPeople();
                     long fin = System.nanoTime();
                     tempsAddpeople += (fin - deb);
                 }
-
-                System.out.println(i + " Addpeople \t" + (tempsAddpeople) / 1e6 + " ");
+                System.out.println(i + " Addpeople \t" + (tempsAddpeople) / 1e6 + "ms");
             }
-
         }
 
-        // methode isolate
-
-        final int MINI = 100_000, MAXI = 500_000, PASI = 100_000;
-        final int MINIS = 1_000, MAXIS = 5_000, PASIS = 1_000;
-        for (int n = MINI; n <= MAXI; n += PASI) {
+        //Mesure de la méthode Isolated -> isolé des habitants
+        final int MINISO = 100_000, MAXISO = 500_000, PASISO = 100_000;
+        final int MINREPISO= 1_000, MAXREPISO = 5_000, PASREPISO = 1_000;
+        for (n = MINISO; n <= MAXISO; n += PASISO) {
             long tempsIsolate = 0;
             UnionFind u = new UnionFind(n);
-            for (int i = MINIS; i <= MAXIS; i += PASIS) {
-
-                for (int j = 0; j < i; j++) {
+            System.out.println("Pour une taille " + n + " : ");
+            for (i = MINREPISO; i <= MAXREPISO; i += PASREPISO) {
+                for (j = 0; j < i; j++) {
                     int hab = r.nextInt(n);
                     long start = System.nanoTime();
                     u.isolated(hab);
                     long end = System.nanoTime();
                     tempsIsolate += (end - start);
                 }
-
                 System.out.println(i + " Isolate \t" + (tempsIsolate) / 1e6 + "ms");
             }
         }
-
-*/
-        /*********************** DEBUT TEST MEMOIRE OPTIMISE ***********************/
-
-        final int MIN = 2_000_000, MAX = 10_000_000, PAS = 2_000_000;
-
-        final int MINREP = 200_000, MAXREP = 1_000_000, PASREP = 200_000;
+        /*********************** FIN TEST TEMPS ***********************/
 
 
+        /*********************** DEBUT TEST MEMOIRE ***********************/
+        final int MINMEMORY = 100_000, MAXMEMORY = 500_000, PASMEMORY = 100_000;
+        final int MINREP = 1_000, MAXREP = 5_000, PASREP = 1_000;
 
-        Random r = new Random();
-        int j = 0;
-
-        for (int n = MIN; n <= MAX; n += PAS) {
-            Observer ob = new Observer();
-            UnionFind u = new UnionFind(n, ob);
+        for (n = MINMEMORY; n <= MAXMEMORY; n += PASMEMORY) {
+            UnionFindOB u = new UnionFindOB(n);
             System.out.println("Pour une taille " + n + " : nombre de variable déclaré  : taille max de la pile d'appels ");
 
-
-
             // Test de la méthode union -> Unir des habitants
-            for (int i = MINREP; i <= MAXREP; i += PASREP) {
+            for (i = MINREP; i <= MAXREP; i += PASREP) {
                 for (j = 0; j < i; j++) {
                     int hab1 = r.nextInt(n);
                     int hab2 = r.nextInt(n);
                     u.union(hab1, hab2);
                 }
-                System.out.println(i + " union \t" + ob + " ");
+                System.out.println(i + " union \t" + u + " ");
             }
 
-            ob = new Observer();
-            u = new UnionFind(n, ob);
+            u = new UnionFindOB(n);
 
             // Test de la méthode find -> Trouver le représentant
-            for (int i = MINREP; i <= MAXREP; i += PASREP) {
+            for (i = MINREP; i <= MAXREP; i += PASREP) {
                 for (j = 0; j < i; j++) {
                     int habfind = r.nextInt(n);
                     u.find(habfind);
                 }
-                System.out.println(i + " find\t" + ob + " ");
+                System.out.println(i + " find\t" + u + " ");
             }
 
-            ob = new Observer();
-            u = new UnionFind(n, ob);
+            u = new UnionFindOB(n);
 
             // Test de la méthode addPeople -> Ajout d'habitant
-            for (int i = MINREP; i <= MAXREP; i += PASREP) {
+            for (i = MINREP; i <= MAXREP; i += PASREP) {
                 for (j = 0; j < i; j++) {
                     u.addPeople();
                 }
-                System.out.println(i + " addpeople\t" + ob + " ");
+                System.out.println(i + " addpeople\t" + u + " ");
             }
 
-
-        }
-
-        final int MINI = 100_000, MAXI = 500_000, PASI = 100_000;
-
-        final int MINIS = 1_000, MAXIS = 5_000, PASIS = 1_000;
-
-        for (int n = MINI; n <= MAXI; n += PASI) {
-            Observer ob = new Observer();
-            UnionFind u = new UnionFind(n, ob);
-            System.out.println("Pour une taille " + n + " : nombre de variable déclaré  : taille max de la pile d'appels ");
+            u = new UnionFindOB(n);
 
             // Test de la méthode isolated -> isolé des habitants
-            for (int i = MINIS; i <= MAXIS; i += PASIS) {
+            for (i = MINREP; i <= MAXREP; i += PASREP) {
                 for (j = 0; j < i; j++) {
                     int hab = r.nextInt(n);
                     u.isolated(hab);
                 }
-                System.out.println(i + " isolate \t" + ob + " ");
+                System.out.println(i + " isolate \t" + u + " ");
             }
         }
-        /*********************** FIN TEST MEMOIRE OPTIMISE ***********************/
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        //Test compléxité moyenne en temps de la méthode find
-        final int MIN = 1_000_000, MAX = 10_000_000, PAS = 1_000_000;
-
-        Random r = new Random();
-        Random r2 = new Random();
-        for (int n = MIN; n <= MAX; n += PAS) {
-            UnionFind u = new UnionFind(n);
-            long temps = 0;
-            //int NB = 100;
-            int NB = r2.nextInt(n) + 1;
-            for (int i = 0; i < NB; i++) {
-                int hab1 = r.nextInt(n);
-                int hab2 = r.nextInt(n);
-                u.union(hab1, hab2);
-
-            }
-            for (int i = 0; i < NB; i++) {
-                long deb = System.nanoTime();
-                u.find(r.nextInt(n));
-                long fin = System.nanoTime();
-                temps += (fin - deb);
-            }
-            System.out.println(n + "\t" + ((temps / NB)) / 1e6);
-
-
-        }*/
-
-        /*
-        //Test compléxité moyenne en mémoire de la méthode find
-        final int MIN = 1_000_000, MAX = 10_000_000, PAS = 1_000_000;
-
-        Random r = new Random();
-        Random r2 = new Random();
-        for (int n = MIN; n <= MAX; n += PAS) {
-             int NB = r2.nextInt(n) + 1;
-            Observer ob = new Observer(NB);
-            UnionFind u = new UnionFind(n, ob);
-            long temps = 0;
-            //int NB = 100;
-            for (int i = 0; i < NB; i++) {
-                int hab1 = r.nextInt(n);
-                int hab2 = r.nextInt(n);
-                u.union(hab1, hab2);
-
-            }
-            for (int i = 0; i < NB; i++) {
-                u.find(r.nextInt(n));
-            }
-            System.out.println(n + "\t" + ob);
-
-
-        }
-         
-        /*
-        //Test compléxité moyenne en temps de la méthode Union
-        final int MIN = 2_000_000, MAX = 10_000_000, PAS = 2_000_000;
-        final int MIN1 = 200_000, MAX1 = 1_000_000, PAS1 = 200_000;
-
-        Random r = new Random();
-        for (int n = MIN; n <= MAX; n += PAS) {
-            UnionFind u = new UnionFind(n);
-            long temps = 0;
-            System.out.println("Pour une taille " + n + " : ");
-            int j;
-            for( int i = MIN1; i <= MAX1; i+=PAS1){
-                for(j = 0; j<i; j++){
-                    int hab = r.nextInt(n);
-                    int hab2 = r.nextInt(n);
-                    u.union(hab,hab2);
-                }
-                for(j = 0; j<i; j++){
-                    long deb = System.nanoTime();
-                    u.addPeople();
-                    long fin = System.nanoTime();
-                    temps += (fin - deb);
-                }
-                if(j < i-1)
-                    System.out.print(i + "\t" + (temps)/1e6 + " ");
-                else
-                    System.out.println(i + "\t" + (temps)/1e6 + " ");
-            }
-          }
-        */
-            }
-        }
+        /*********************** FIN TEST MEMOIRE ***********************/
+    }
+}
--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,20 +1,8 @@
 import java.util.ArrayList;
 
 public class UnionFind {
-
     private ArrayList<Integer> amis;
     private int nbHabitant;
-
-    private static Observer o;
-
-    public UnionFind(int nbH, Observer ob){
-        this.nbHabitant = nbH;
-        this.amis = new ArrayList<>(this.nbHabitant);
-        for(int i = 0; i < this.nbHabitant; ++i){
-            this.amis.add(i);
-        }
-        o = ob;
-    }
 
     public UnionFind(int nbH){
         this.nbHabitant = nbH;
@@ -22,86 +10,76 @@ public class UnionFind {
         for(int i = 0; i < this.nbHabitant; ++i){
             this.amis.add(i);
         }
-        o = null;
     }
 
     public int find (int habitant) {
-       //o.enter();
-       //o.incOp();
         int r = habitant;
         while (this.amis.get(r) != r) {
             r = this.amis.get(r);
         }
-        //o.incOp();
         int a = habitant;
-        //o.incOp();
         int tmp;
         while (this.amis.get(a) != a) {
             tmp = a;
             a = this.amis.get(tmp);
             this.amis.set(tmp, r);
         }
-      // o.exit();
         return r;
-
     }
 
     public void union (int habitant1, int habitant2){
-        //o.enter();
-        //o.incOp();
         int rep1 = this.find(habitant1);
-        //o.incOp();
         int rep2 = this.find(habitant2);
-            if(rep1 < rep2) this.amis.set(habitant2, rep1);
-        //o.exit();
+        //Condition pour afecter l'habitant le plus petit facilitant pour la suite les possible find
+        if(rep1 < rep2) this.amis.set(habitant2, rep1);
     }
 
     public void addPeople() {
         ++this.nbHabitant;
         if (this.nbHabitant > this.amis.size()) {
+            //Garantit une certaine taille à la liste, modifie sa capacité interne
+            // afin de pouvoir ajouter un grand nombre sans crash
             this.amis.ensureCapacity(this.nbHabitant);
         }
         this.amis.add(nbHabitant - 1);
     }
 
 
+    //Isole un habitant
     public void isolated(int habitant) {
         int representant = find(habitant);
+        int i;
+        boolean foundAnotherRepresentant = false;
 
-        if (representant == habitant) {
-            // Si habitant est son propre représentant, cherchons un autre représentant parmi ses amis
-            boolean foundAnotherRepresentant = false;
+        for (i = 0; i < this.nbHabitant; ++i) {
+            int friend = this.amis.get(i);
 
-            for (int i = 0; i < this.nbHabitant; ++i) {
-                int friend = this.amis.get(i);
-
-                if (friend == habitant && i != habitant) {
-                    if (!foundAnotherRepresentant) {
-                        representant = i;
-                        foundAnotherRepresentant = true;
-                    }
-                    this.amis.set(i, representant);
+            //Désigne un nouveau représentant au groupe d'amis
+            if (friend == habitant && i != habitant) {
+                if (!foundAnotherRepresentant) {
+                    representant = i;
+                    foundAnotherRepresentant = true;
                 }
             }
-        }
 
-        // Mettre à jour les amis de habitant pour qu'ils pointent vers le représentant
-        for (int i = 0; i < this.nbHabitant; ++i) {
-            if (this.amis.get(i) == habitant && i != habitant) {
+            //Affecte le nouveau représentant dans le groupe d'amis
+            if (friend == habitant && i != habitant) {
                 this.amis.set(i, representant);
             }
         }
 
-        // Si habitant n'est pas déjà son propre représentant, le définir comme tel
+        //En cas ou l'habitant ne serait pas son propre représentant devient son propre représentant
         if (this.amis.get(habitant) != habitant) {
             this.amis.set(habitant, habitant);
         }
     }
+
+    //Affiche le tableau
     public String toString (){
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for(int i = 0; i < this.nbHabitant; ++i){
-            if(i != this.nbHabitant - 1) sb.append(this.amis.get(i)+", ");
+            if(i != this.nbHabitant - 1) sb.append(this.amis.get(i) + ", ");
             else sb.append(this.amis.get(i));
         }
         sb.append("]");
